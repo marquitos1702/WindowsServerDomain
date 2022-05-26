bac34fbe4db7550ab9f2f193ffe50e1f02980866
marquitos1702 <106312022+marquitos1702@users.noreply.github.com>
2022-05-26 17:28:46 +0200

    Update creation.ps1

--- creation.ps1
+++ creation.ps1
@@ -9,16 +9,24 @@ Install-WindowsFeature -Name AD-DomainServices -IncludeManagementTools
 #The second step: 
 
    Install-ADDSForest `
-  -DomainName "example.com" 
-  -CreateDnsDelegation:$false 
-  -DatabasePath "C:\Windows\NTDS" 
-  -DomainMode "7"  
-  -DomainNetbiosName "Example" 
-  -ForestMode "7"  
-  -InstallDns:$true  
-  -LogPath "C:\Windows\NTDS"  
-  -NoRebootOnCompletion:$True  
-  -SysvolPath "C:\Windows\SYSVOL" 
-  -Force:$true
-# The third step: 
-
+  -DomainName "example.com" `
+  -CreateDnsDelegation:$false `  
+  -DatabasePath "C:\Windows\NTDS" ` 
+  -DomainMode "WinThreshold" `  
+  -DomainNetbiosName "Example" ` 
+  -ForestMode "WinThreshold" ` 
+  -InstallDns:$true ` 
+  -LogPath "C:\Windows\NTDS" `  
+  -NoRebootOnCompletion:$True `  
+  -SysvolPath "C:\Windows\SYSVOL" `
+  -Force:$true `
+# The third step: install the features ()
+Install-WindowsFeature -Name <feature_name>  -Restart
+Install-WindowsFeature -Name <feature_name>  -Restart
+Install-WindowsFeature -Name <feature_name>  -Restart
+Install-WindowsFeature -Name <feature_name>  -Restart
+Install-WindowsFeature -Name <feature_name>  -Restart
+Install-WindowsFeature -Name <feature_name>  -Restart
+Install-WindowsFeature -Name <feature_name>  -Restart
+Install-WindowsFeature -Name <feature_name>  -Restart
+Install-WindowsFeature -Name <feature_name>  -Restart
