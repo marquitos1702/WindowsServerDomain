e5284f227d6d44cf3db05539b0f6d91469b02977
marquitos1702 <106312022+marquitos1702@users.noreply.github.com>
2022-05-26 13:56:55 +0200

    Update creation.ps1

--- creation.ps1
+++ creation.ps1
@@ -1,8 +1,4 @@
-#The first steps to do a domain in Windows Server are :
-  
-  # Set a static ip in your Server
-  
-  NewNetIPAddress InterfaceIndex 4 -IPAddress 192.168.1.8-PrefixLenght 24 DefaultGateway 192.168.1.1
+#The first is reading the README document:
   
 #Steps to do a domain in Windows Server are :
 
