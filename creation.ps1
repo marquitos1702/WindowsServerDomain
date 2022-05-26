ec4cb1124b7e7df0f2ded73384846515c9d3530c
marquitos1702 <106312022+marquitos1702@users.noreply.github.com>
2022-05-26 16:07:23 +0200

    Update creation.ps1

--- creation.ps1
+++ creation.ps1
@@ -8,3 +8,17 @@ Install-WindowsFeature -Name AD-DomainServices -IncludeManagementTools
 
 #The second step: 
 
+   Install-ADDSForest `
+  -DomainName "example.com" 
+  -CreateDnsDelegation:$false 
+  -DatabasePath "C:\Windows\NTDS" 
+  -DomainMode "7"  
+  -DomainNetbiosName "Example" 
+  -ForestMode "7"  
+  -InstallDns:$true  
+  -LogPath "C:\Windows\NTDS"  
+  -NoRebootOnCompletion:$True  
+  -SysvolPath "C:\Windows\SYSVOL" 
+  -Force:$true
+# The third step: 
+
